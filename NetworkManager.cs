de6e2f45e07aa0aea5f0c7683cc4cdadac8a790e
ShyBL <rakesh1985@gmail.com>
2024-04-10 13:19:07 +0300

    Bug Fix

--- NetworkManager.cs
+++ NetworkManager.cs
@@ -2,105 +2,99 @@
 using UnityEngine;
 using Colyseus;
 
-namespace Starwards
+public class NetworkManager : MonoBehaviour
 {
-    public class NetworkManager : MonoBehaviour
+    public Dictionary<string, SpaceObjectBase> Entities = new ();
+    public Dictionary<string, NetworkedEntity> NetworkedEntities = new ();
+    
+    private static ColyseusClient _client = null;
+    private static ColyseusRoom<SpaceState> _spaceRoom = null;
+
+    public string endPoint = "ws://localhost:80";
+    public string adminRoomName = "admin";
+    public string spaceRoomName = "space";
+    public string shipRoomName = "ship";
+
+    public static NetworkManager Instance;
+
+    private void Awake()
     {
-        public Dictionary<string, SpaceObjectBase> entities = new ();
-        public Dictionary<string, NetworkedEntity> networkedEntities = new ();
-
-        
-        private static ColyseusClient _client = null;
-        private static ColyseusRoom<SpaceState> _spaceRoom = null;
-
-        public string endPoint = "ws://localhost:80";
-        public string adminRoomName = "admin";
-        public string spaceRoomName = "space";
-        public string shipRoomName = "ship";
-
-        public static NetworkManager Instance;
-
-        private void Awake()
+        if (Instance == null)
         {
-            if (Instance == null)
-            {
-                Instance = this;
-            }
-            
-            else
-            {
-                Debug.LogError($"Two {typeof(NetworkManager)} instances exist, didn't create new one");
-            }
+            Instance = this;
         }
-
-        private async void Start()
+            
+        else
         {
-            Initialize();
-            _spaceRoom = await _client.Join<SpaceState>(spaceRoomName);
-            Debug.Log($"connected: {_spaceRoom.RoomId}");
-        }
-        
-        private void Initialize()
-        {
-            _client = new ColyseusClient(endPoint);
-        }
-
-        private void Update()
-        {
-            if (_spaceRoom != null)
-            {
-                SpaceRoomUpdate(_spaceRoom.State);
-            }
-        }
-
-        private void SpaceRoomUpdate(SpaceState state)
-        {
-            if (state.Asteroid.Count != 0)
-            {
-                state.Asteroid.ForEach(UpdateObjects);
-            }
-            
-            if (state.Spaceship.Count != 0)
-            {
-                state.Spaceship.ForEach(UpdateObjects);
-            }
-            
-            if (state.Waypoint.Count != 0)
-            {
-                
-            }
-            
-            if (state.Projectile.Count != 0)
-            {
-                
-            }
-            
-            if (state.Explosion.Count != 0)
-            {
-                
-            }
-        }
-
-        private void UpdateObjects(string s, SpaceObjectBase spaceObject)
-        {
-            if (spaceObject.destroyed) return;
-            
-            if (entities.ContainsKey(spaceObject.id))
-            {
-                networkedEntities[spaceObject.id].Move();
-                Debug.Log($"Moved {s}");
-            }
-            
-            else if (!entities.ContainsKey(spaceObject.id))
-            {
-                var parentObject = new GameObject();
-                parentObject.name = $"{s}";
-                var NetworkedEntity = parentObject.AddComponent<NetworkedEntity>();
-                
-                NetworkedEntity.Init(spaceObject,parentObject);
-                Debug.Log($"Spawned {s}");
-            }
+            Debug.LogError($"Two {typeof(NetworkManager)} instances exist, didn't create new one");
         }
     }
-    
+
+    private async void Start()
+    {
+        Initialize();
+        _spaceRoom = await _client.Join<SpaceState>(spaceRoomName);
+        Debug.Log($"connected: {_spaceRoom.RoomId}");
+    }
+        
+    private void Initialize()
+    {
+        _client = new ColyseusClient(endPoint);
+    }
+
+    private void Update()
+    {
+        if (_spaceRoom != null)
+        {
+            SpaceRoomUpdate(_spaceRoom.State);
+        }
+    }
+
+    private void SpaceRoomUpdate(SpaceState state)
+    {
+        if (state.Asteroid.Count != 0)
+        {
+            state.Asteroid.ForEach(UpdateObjects);
+        }
+            
+        if (state.Spaceship.Count != 0)
+        {
+            state.Spaceship.ForEach(UpdateObjects);
+        }
+            
+        if (state.Waypoint.Count != 0)
+        {
+                
+        }
+            
+        if (state.Projectile.Count != 0)
+        {
+                
+        }
+            
+        if (state.Explosion.Count != 0)
+        {
+                
+        }
+    }
+
+    private void UpdateObjects(string s, SpaceObjectBase spaceObject)
+    {
+        if (spaceObject.destroyed) return;
+            
+        if (Entities.ContainsKey(spaceObject.id))
+        {
+            NetworkedEntities[spaceObject.id].Move(spaceObject);
+            Debug.Log($"Moved {s}");
+        }
+        else 
+        {
+            var parentObject = new GameObject();
+            parentObject.name = $"{s}";
+            var NetworkedEntity = parentObject.AddComponent<NetworkedEntity>();
+                
+            NetworkedEntity.Init(spaceObject,parentObject);
+            Debug.Log($"Spawned {s}");
+        }
+    }
 }
--- NetworkedEntities/NetworkedEntity.cs
+++ NetworkedEntities/NetworkedEntity.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
-namespace Starwards
-{
     public class NetworkedEntity : MonoBehaviour
     {
         public void Init(SpaceObjectBase state, GameObject parent)
@@ -11,59 +8,61 @@ namespace Starwards
             switch (state)
             {
                 case Spaceship spaceship:
-                    var Spaceship = (GameObject)Instantiate(Resources.Load("SpaceshipPrefab"),new Vector3(spaceship.position.x,spaceship.position.y),Quaternion.identity);
-                    Spaceship.transform.SetParent(parent.transform);
                     
-                    NetworkManager.Instance.entities.Add(spaceship.id,spaceship);
-                    NetworkManager.Instance.networkedEntities.Add(spaceship.id,this);
-
-                    if (spaceship.faction == 0)
+                    if (spaceship.id == "")
                     {
-                        Camera.main.transform.SetParent(Spaceship.transform);
+                        var Spaceship = (GameObject)Instantiate(Resources.Load("PlayerPrefab"),NewPosition(spaceship),Quaternion.identity);
+                        Spaceship.transform.SetParent(parent.transform);
+                    }
+                    if (spaceship.id == "")
+                    {
+                        var Spaceship = (GameObject)Instantiate(Resources.Load("SpaceshipPrefab"),NewPosition(spaceship),Quaternion.identity);
+                        Spaceship.transform.SetParent(parent.transform);
                     }
                     
+                    AddToControlList(spaceship);
                     break;
                 
                 case Asteroid asteroid:
-                    var Asteroid = (GameObject)Instantiate(Resources.Load("AsteroidPrefab"),new Vector3(asteroid.position.x,asteroid.position.x),Quaternion.identity);
+                    
+                    var Asteroid = (GameObject)Instantiate(Resources.Load("AsteroidPrefab"), NewPosition(asteroid),Quaternion.identity);
                     Asteroid.transform.SetParent(parent.transform);
                     
-                    NetworkManager.Instance.entities.Add(asteroid.id,asteroid);
-                    NetworkManager.Instance.networkedEntities.Add(asteroid.id,this);
+                    AddToControlList(asteroid);
                     break;
                 
                 case Explosion explosion:
-                    var Explosion = (GameObject)Instantiate(Resources.Load("ExplosionPrefab"),new Vector3(explosion.position.x,explosion.position.x),Quaternion.identity);
-                    Explosion.transform.SetParent(parent.transform);
-                    
-                    NetworkManager.Instance.entities.Add(explosion.id,explosion);
-                    NetworkManager.Instance.networkedEntities.Add(explosion.id,this);
-                    break;
-                
                 case Projectile projectile:
-                    var Projectile = (GameObject)Instantiate(Resources.Load("ProjectilePrefab"),new Vector3(projectile.position.x,projectile.position.x),Quaternion.identity);
-                    Projectile.transform.SetParent(parent.transform);
-                    
-                    NetworkManager.Instance.entities.Add(projectile.id,projectile);
-                    NetworkManager.Instance.networkedEntities.Add(projectile.id,this);
-                    break;
-                
                 case Waypoint waypoint:
-                    var Waypoint = (GameObject)Instantiate(Resources.Load("WaypointPrefab"),new Vector3(waypoint.position.x,waypoint.position.x),Quaternion.identity);
-                    Waypoint.transform.SetParent(parent.transform);
                     
-                    NetworkManager.Instance.entities.Add(waypoint.id,waypoint);
-                    NetworkManager.Instance.networkedEntities.Add(waypoint.id,this);
+                    // var Waypoint = (GameObject)Instantiate(Resources.Load("WaypointPrefab"),new Vector3(waypoint.position.x,waypoint.position.x),Quaternion.identity);
+                    // Waypoint.transform.SetParent(parent.transform);
+                    //AddToControlList(waypoint);
                     break;
-                
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(state));
             }
         }
-        public void Move()
+
+        private Vector3 NewPosition(SpaceObjectBase spaceObject)
         {
-            
+            var newPos = new Vector3(spaceObject.position.x, 0, spaceObject.position.y);
+            return newPos;
         }
-        
+        private void AddToControlList(SpaceObjectBase spaceObject)
+        {
+            NetworkManager.Instance.Entities.Add(spaceObject.id, spaceObject);
+            NetworkManager.Instance.NetworkedEntities.Add(spaceObject.id, this);
+        }
+
+        public void Move(SpaceObjectBase spaceObject)
+        {
+            gameObject.transform.position = new Vector3(spaceObject.position.x,spaceObject.position.y, 0);
+            
+            var deg = spaceObject.angle;
+            
+            Quaternion target = Quaternion.Euler(0,deg,0);
+            
+            gameObject.transform.localRotation = target;
+
+
+        }
     }
-}
